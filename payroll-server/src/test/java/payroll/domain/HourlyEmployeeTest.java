0d1ab281dc0b46f888ce44328c62518f47f9187b
文野 <zhang_colin@163.com>
2019-11-29 22:38:21 +0800

    add new test for empty timecard

--- payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
+++ payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
@@ -3,6 +3,7 @@ package payroll.domain;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -15,11 +16,12 @@ public class HourlyEmployeeTest {
     private final Period settlementPeriod = new Period(
             LocalDate.of(2019, 11, 4),
             LocalDate.of(2019, 11, 8));
+    private final Money salaryOfHour = Money.of(100, Currency.RMB);
+
     @Test
     public void should_calculate_payroll_by_work_hours_in_a_week() {
         // given
         final List<TimeCard> timeCards = createTimeCards(8, 8, 8, 8, 8);
-        final Money salaryOfHour = Money.of(100, Currency.RMB);
         final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
 
         // when
@@ -36,7 +38,6 @@ public class HourlyEmployeeTest {
     public void should_calculate_payroll_by_work_hours_with_overtime_in_a_week() {
         // given
         final List<TimeCard> timeCards = createTimeCards(9, 7, 10, 10, 8);
-        final Money salaryOfHour = Money.of(100, Currency.RMB);
         final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
 
         // when
@@ -47,6 +48,19 @@ public class HourlyEmployeeTest {
         assertThat(payroll.getBeginDate()).isEqualTo(LocalDate.of(2019, 11, 4));
         assertThat(payroll.getEndDate()).isEqualTo(LocalDate.of(2019, 11, 8));
         assertThat(payroll.getAmount()).isEqualTo(Money.of(4650, Currency.RMB));
+    }
+
+    @Test
+    public void should_be_0_given_no_any_time_card() {
+        // given
+        final HourlyEmployee hourlyEmployee = new HourlyEmployee(new ArrayList<>(), salaryOfHour);
+
+        // when
+        Payroll payroll = hourlyEmployee.payroll(settlementPeriod);
+
+        // then
+        assertThat(payroll).isNotNull();
+        assertThat(payroll.getAmount()).isEqualTo(Money.of(0.00, Currency.RMB));
     }
 
     private List<TimeCard> createTimeCards(int workHours1, int workHours2, int workHours3, int workHours4, int workHours5) {
