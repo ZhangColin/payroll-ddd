3f59e6e63f66154f2fbb530a371f2157ed8db7a7
文野 <zhang_colin@163.com>
2019-11-29 16:01:17 +0800

    refactor current test

--- payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
+++ payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
@@ -15,15 +15,9 @@ public class HourlyEmployeeTest {
     @Test
     public void should_calculate_payroll_by_work_hours_in_a_week() {
         // given
-        final TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 11, 4), 8);
-        final TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 11, 5), 8);
-        final TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 11, 6), 8);
-        final TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 11, 7), 8);
-        final TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 11, 8), 8);
-
-        final List<TimeCard> timeCards = asList(timeCard1, timeCard2, timeCard3, timeCard4, timeCard5);
-
-        final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, Money.of(10000, Currency.RMB));
+        final List<TimeCard> timeCards = createTimeCards();
+        final Money salaryOfHour = Money.of(10000, Currency.RMB);
+        final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
 
         // when
         Payroll payroll = hourlyEmployee.payroll();
@@ -33,5 +27,15 @@ public class HourlyEmployeeTest {
         assertThat(payroll.getBeginDate()).isEqualTo(LocalDate.of(2019, 11, 4));
         assertThat(payroll.getEndDate()).isEqualTo(LocalDate.of(2019, 11, 8));
         assertThat(payroll.getAmount()).isEqualTo(Money.of(400000, Currency.RMB));
+    }
+
+    private List<TimeCard> createTimeCards() {
+        final TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 11, 4), 8);
+        final TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 11, 5), 8);
+        final TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 11, 6), 8);
+        final TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 11, 7), 8);
+        final TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 11, 8), 8);
+
+        return asList(timeCard1, timeCard2, timeCard3, timeCard4, timeCard5);
     }
 }
