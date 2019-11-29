8805655b47f21125ab4cee4ca2007e56a7b7aac5
文野 <zhang_colin@163.com>
2019-11-29 16:07:58 +0800

    extract parameter for createTimeCards();write a new test for overtime case

--- payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
+++ payroll-server/src/test/java/payroll/domain/HourlyEmployeeTest.java
@@ -15,7 +15,7 @@ public class HourlyEmployeeTest {
     @Test
     public void should_calculate_payroll_by_work_hours_in_a_week() {
         // given
-        final List<TimeCard> timeCards = createTimeCards();
+        final List<TimeCard> timeCards = createTimeCards(8, 8, 8, 8, 8);
         final Money salaryOfHour = Money.of(10000, Currency.RMB);
         final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
 
@@ -29,12 +29,29 @@ public class HourlyEmployeeTest {
         assertThat(payroll.getAmount()).isEqualTo(Money.of(400000, Currency.RMB));
     }
 
-    private List<TimeCard> createTimeCards() {
-        final TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 11, 4), 8);
-        final TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 11, 5), 8);
-        final TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 11, 6), 8);
-        final TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 11, 7), 8);
-        final TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 11, 8), 8);
+    @Test
+    public void should_calculate_payroll_by_work_hours_with_overtime_in_a_week() {
+        // given
+        final List<TimeCard> timeCards = createTimeCards(9, 7, 10, 10, 8);
+        final Money salaryOfHour = Money.of(10000, Currency.RMB);
+        final HourlyEmployee hourlyEmployee = new HourlyEmployee(timeCards, salaryOfHour);
+
+        // when
+        Payroll payroll = hourlyEmployee.payroll();
+
+        // then
+        assertThat(payroll).isNotNull();
+        assertThat(payroll.getBeginDate()).isEqualTo(LocalDate.of(2019, 11, 4));
+        assertThat(payroll.getEndDate()).isEqualTo(LocalDate.of(2019, 11, 8));
+        assertThat(payroll.getAmount()).isEqualTo(Money.of(465000, Currency.RMB));
+    }
+
+    private List<TimeCard> createTimeCards(int workHours1, int workHours2, int workHours3, int workHours4, int workHours5) {
+        final TimeCard timeCard1 = new TimeCard(LocalDate.of(2019, 11, 4), workHours1);
+        final TimeCard timeCard2 = new TimeCard(LocalDate.of(2019, 11, 5), workHours2);
+        final TimeCard timeCard3 = new TimeCard(LocalDate.of(2019, 11, 6), workHours3);
+        final TimeCard timeCard4 = new TimeCard(LocalDate.of(2019, 11, 7), workHours4);
+        final TimeCard timeCard5 = new TimeCard(LocalDate.of(2019, 11, 8), workHours5);
 
         return asList(timeCard1, timeCard2, timeCard3, timeCard4, timeCard5);
     }
