49343c8f9137aa5208214a9a51f528e05d7c9e64
文野 <zhang_colin@163.com>
2019-12-01 06:16:12 +0800

    extract some fields of test class;extract assertion method to reuse;add new test

--- payroll-server/src/test/java/payroll/domain/HourlyEmployeePayrollCalculatorTest.java
+++ payroll-server/src/test/java/payroll/domain/HourlyEmployeePayrollCalculatorTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static payroll.fixture.EmployeeFixture.createHourlyEmployee;
@@ -16,21 +17,21 @@ public class HourlyEmployeePayrollCalculatorTest {
 
     private Period settlementPeriod;
     private HourlyEmployeeRepository mockRepository;
+    private HourlyEmployeePayrollCalculator calculator;
 
     @Before
     public void setUp() throws Exception {
         settlementPeriod = new Period(LocalDate.of(2019, 11, 4),
                 LocalDate.of(2019, 11, 8));
         mockRepository = mock(HourlyEmployeeRepository.class);
+        calculator = new HourlyEmployeePayrollCalculator();
+        calculator.setRepository(mockRepository);
     }
 
     @Test
     public void should_calculate_payroll_when_no_matched_employee_found() {
         // given
         when(mockRepository.allEmployeesOf(settlementPeriod)).thenReturn(new ArrayList<>());
-
-        final HourlyEmployeePayrollCalculator calculator = new HourlyEmployeePayrollCalculator();
-        calculator.setRepository(mockRepository);
 
         // when
         final List<Payroll> payrolls = calculator.execute(settlementPeriod);
@@ -45,12 +46,9 @@ public class HourlyEmployeePayrollCalculatorTest {
         // given
         String employeeId = "emp200901011111";
         final HourlyEmployee hourlyEmployee = createHourlyEmployee(employeeId, 8, 8, 8, 8, 8);
-        final List<HourlyEmployee> hourlyEmployees = asList(hourlyEmployee);
+        final List<HourlyEmployee> hourlyEmployees = singletonList(hourlyEmployee);
 
         when(mockRepository.allEmployeesOf(settlementPeriod)).thenReturn(hourlyEmployees);
-
-        final HourlyEmployeePayrollCalculator calculator = new HourlyEmployeePayrollCalculator();
-        calculator.setRepository(mockRepository);
 
         // when
         final List<Payroll> payrolls = calculator.execute(settlementPeriod);
@@ -58,11 +56,38 @@ public class HourlyEmployeePayrollCalculatorTest {
         // then
         verify(mockRepository, times(1)).allEmployeesOf(settlementPeriod);
         assertThat(payrolls).isNotNull().hasSize(1);
+        assertPayroll(employeeId, payrolls, 0, 4000);
+    }
 
-        final Payroll payroll = payrolls.get(0);
+    @Test
+    public void should_calculate_payroll_when_more_than_one_matched_employee_found() {
+        // given
+        String employeeId1 = "emp200901011111";
+        final HourlyEmployee hourlyEmployee1 = createHourlyEmployee(employeeId1, 8, 8, 8, 8, 8);
+        String employeeId2 = "emp200901011112";
+        final HourlyEmployee hourlyEmployee2 = createHourlyEmployee(employeeId2, 9, 7, 10, 10, 8);
+        String employeeId3 = "emp200901011113";
+        final HourlyEmployee hourlyEmployee3 = createHourlyEmployee(employeeId3, null);
+        final List<HourlyEmployee> hourlyEmployees = asList(hourlyEmployee1, hourlyEmployee2, hourlyEmployee3);
+
+        when(mockRepository.allEmployeesOf(settlementPeriod)).thenReturn(hourlyEmployees);
+
+        // when
+        final List<Payroll> payrolls = calculator.execute(settlementPeriod);
+
+        // then
+        verify(mockRepository, times(1)).allEmployeesOf(settlementPeriod);
+        assertThat(payrolls).isNotNull().hasSize(3);
+        assertPayroll(employeeId1, payrolls, 0, 4000);
+        assertPayroll(employeeId2, payrolls, 1, 4650);
+        assertPayroll(employeeId3, payrolls, 2, 0);
+    }
+
+    private void assertPayroll(String employeeId, List<Payroll> payrolls, int index, int payrollAmount) {
+        final Payroll payroll = payrolls.get(index);
         assertThat(payroll.getEmployeeId()).isEqualTo(employeeId);
         assertThat(payroll.getBeginDate()).isEqualTo(LocalDate.of(2019, 11, 4));
         assertThat(payroll.getEndDate()).isEqualTo(LocalDate.of(2019, 11, 8));
-        assertThat(payroll.getAmount()).isEqualTo(Money.of(4000, Currency.RMB));
+        assertThat(payroll.getAmount()).isEqualTo(Money.of(payrollAmount, Currency.RMB));
     }
 }
