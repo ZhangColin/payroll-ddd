0ba29ee8b2339c46ca81c2b8ddf6d082c43ca680
文野 <zhang_colin@163.com>
2019-12-01 15:03:24 +0800

    check currency of money

--- payroll-server/src/main/java/payroll/payrollcontext/domain/Money.java
+++ payroll-server/src/main/java/payroll/payrollcontext/domain/Money.java
@@ -1,5 +1,7 @@
 package payroll.payrollcontext.domain;
 
+import payroll.payrollcontext.domain.exceptions.NotSameCurrencyException;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Objects;
@@ -12,6 +14,14 @@ public class Money {
 
     private final BigDecimal value;
     private final Currency currency;
+
+    public static Money of(double value) {
+        return new Money(value, Currency.RMB);
+    }
+
+    public static Money of(double value, Currency currency) {
+        return new Money(value, currency);
+    }
 
     private Money(double value, Currency currency) {
         this.value = BigDecimal.valueOf(value).setScale(SCALE, RoundingMode.DOWN);
@@ -28,29 +38,28 @@ public class Money {
     }
 
     public Money add(Money money) {
+        throwExceptionIfNotSameCurrency(money);
         return new Money(value.add(money.value).setScale(SCALE, RoundingMode.DOWN), currency);
     }
 
     public Money subtract(Money money) {
+        throwExceptionIfNotSameCurrency(money);
         return new Money(value.subtract(money.value).setScale(SCALE, RoundingMode.DOWN), currency);
     }
 
     public Money multiply(double factor) {
-        final BigDecimal factorDecimal = BigDecimal.valueOf(factor);
-        return new Money(value.multiply(factorDecimal).setScale(SCALE, RoundingMode.DOWN), currency);
+        return new Money(value.multiply(BigDecimal.valueOf(factor))
+                .setScale(SCALE, RoundingMode.DOWN), currency);
     }
 
     public Money divide(double multiplicand) {
-        final BigDecimal divided = BigDecimal.valueOf(multiplicand);
-        return new Money(value.divide(divided, SCALE, RoundingMode.DOWN), currency);
+        return new Money(value.divide(BigDecimal.valueOf(multiplicand), SCALE, RoundingMode.DOWN), currency);
     }
 
-    public static Money of(double value) {
-        return new Money(value, Currency.RMB);
-    }
-
-    public static Money of(double value, Currency currency) {
-        return new Money(value, currency);
+    private void throwExceptionIfNotSameCurrency(Money money) {
+        if (money.currency != this.currency) {
+            throw new NotSameCurrencyException("Don't support different currency.");
+        }
     }
 
     @Override
